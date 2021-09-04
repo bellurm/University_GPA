a00d48fbb384cfb48204e20525ef2e14a839e78b
Mehmetali <72599968+bellurm@users.noreply.github.com>
2021-09-05 00:51:30 +0300

    Grade Point Average Calculater

---
+++ University_GPA/University_GPA/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace University_GPA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.avg_lbl = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.calc_btn = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(6, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Exam:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.groupBox1.Controls.Add(this.avg_lbl);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.calc_btn);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(644, 234);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Exams";
+            // 
+            // avg_lbl
+            // 
+            this.avg_lbl.AutoSize = true;
+            this.avg_lbl.Location = new System.Drawing.Point(427, 178);
+            this.avg_lbl.Name = "avg_lbl";
+            this.avg_lbl.Size = new System.Drawing.Size(67, 26);
+            this.avg_lbl.TabIndex = 5;
+            this.avg_lbl.Text = "label3";
+            this.avg_lbl.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 26;
+            this.listBox1.Items.AddRange(new object[] {
+            "AA: Excellent!",
+            "BA: Congratulations!",
+            "BB: Good!",
+            "CB: Can be better!",
+            "CC: Snap out of it!",
+            "FF: Poor you..."});
+            this.listBox1.Location = new System.Drawing.Point(347, 10);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(291, 212);
+            this.listBox1.TabIndex = 0;
+            // 
+            // calc_btn
+            // 
+            this.calc_btn.Location = new System.Drawing.Point(6, 163);
+            this.calc_btn.Name = "calc_btn";
+            this.calc_btn.Size = new System.Drawing.Size(306, 57);
+            this.calc_btn.TabIndex = 4;
+            this.calc_btn.Text = "Calculate";
+            this.calc_btn.UseVisualStyleBackColor = true;
+            this.calc_btn.Click += new System.EventHandler(this.calc_btn_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(187, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 34);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(187, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 34);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(6, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Second Exam:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(664, 246);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button calc_btn;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label avg_lbl;
+    }
+}
+
---
+++ University_GPA/University_GPA/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace University_GPA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calc_btn_Click(object sender, EventArgs e)
+        {
+            double first, second, average;
+            first = Convert.ToDouble(textBox1.Text);
+            second = Convert.ToDouble(textBox2.Text);
+            average = (first * 0.40 + second * 0.60);
+
+            if(average <= 100 && average >= 90)
+            {
+                avg_lbl.Text = average.ToString() + " >> AA";
+            }
+            else if(average <= 89 && average >= 80)
+            {
+                avg_lbl.Text = average.ToString() + " >> BA";
+            }
+            else if(average <= 79 && average >=70)
+            {
+                avg_lbl.Text = average.ToString() + " >> BB";
+            }
+            else if (average <= 69 && average >= 60)
+            {
+                avg_lbl.Text = average.ToString() + " >> CB";
+            }
+            else if (average <= 59 && average >= 50)
+            {
+                avg_lbl.Text = average.ToString() + " >> CC";
+            }
+            else
+            {
+                avg_lbl.Text = average.ToString() + " >> FF";
+            }
+            avg_lbl.Visible = true;
+        }
+    }
+}
